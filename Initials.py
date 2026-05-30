6d32d1fd904f7a652ba4ff3ea7f9c2c0359d883a
Lily-Amakyewaa-Botwe <2425403099@live.gctu.edu.gh>
2026-05-30 20:51:24 +0000

    Add files via upload

---
+++ Initials.py
@@ -0,0 +1,35 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+# Grader: Mr. Augustus Buckman
+# Name: Lily Amakyewaa Botwe
+# STUDENT ID: 2425403099
+# Email: 2425403099@live.gctu.edu.gh
+
+
+# On my honor, Lily Amakyewaa Botwe, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
+def main():
+    # Prints my initials in large stylized letters
+
+    print()
+    print("...LAB")
+    print()
+
+    print("...LLLLLLLLLLLL....AAAAAAAAAAAA....BBBBBBBBBBBB")
+    print("...LL          ....AA        AA....BB        BB")
+    print("...LL          ....AA        AA....BB        BB")
+    print("...LL          ....AA        AA....BB        BB")
+    print("...LL          ....AAAAAAAAAAAA....BBBBBBBBBBBB")
+    print("...LL          ....AA        AA....BB        BB")
+    print("...LL          ....AA        AA....BB        BB")
+    print("...LL          ....AA        AA....BB        BB")
+    print("...LL          ....AA        AA....BB        BB")
+    print("...LLLLLLLLLLLL****AA        AA****BBBBBBBBBBBB")
+
+    print()
+
+
+main()
